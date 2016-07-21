9a2900c670a0139ee184535a3befee0d4b8da74c
damien10 <damien.batisse01@gmail.com>
2016-07-21 11:07:16 +0200

    modification  agence.php

--- agence.php
+++ agence.php
@@ -6,32 +6,29 @@
   </head>
     <body>
     <!-- Formulaire de choix de voyage -->
-      <form method="post" action="recapitulatif.php">
-        <p>
-          <label for="choix_voyage">Choix du voyage</label>
-          <select name="choix_voyage" id="choix_voyage">
-            <option value="caraibes">Les caraibes - le pied - 4500€ par personnes pour une semaine </option>
-            <option value="canaries">Les canaries - la main - 3500€ par personnes pour une semaine </option>
-          </select>
-        </p>
-        <p>
-          <label for="nbr_personnes">Nombres de personnes</label>
-            <select name="nbr_personnes" id="nbr_personnes">
-              <option value="1">1 personne </option>
-              <option value="2">2 personnes </option>
-              <option value="3">3 personnes </option>
-              <option value="4">4 personnes </option>
-            </select>
-        </p>
-          <input type="submit" name="Estimation" value="Estimation">
-      </form>
-<br>
-<br>
-<br>
+    <?php
+
+      echo '<form method="post" action="recapitulatif.php">';
+          echo '<label for="choix_voyage">Choix du voyage</label>';
+          echo '<select name="choix_voyage" id="choix_voyage">';
+            echo '<option value="caraibes">Les caraibes - le pied - 4500€ par personnes pour une semaine </option>';
+          echo '  <option value="canaries">Les canaries - la main - 3500€ par personnes pour une semaine </option>';
+          echo '</select><br>';
+          echo '<label for="nbr_personnes">Nombres de personnes</label>';
+          echo '  <select name="nbr_personnes" id="nbr_personnes">';
+            echo '  <option value="1">1 personne </option>';
+              echo '<option value="2">2 personnes </option>';
+              echo '<option value="3">3 personnes </option>';
+              echo '<option value="4">4 personnes </option>';
+            echo '</select><br>';
+          echo '<input type="submit" name="Estimation" value="Estimation">';
+      echo '</form>';
 
 
-      <!-- Formulaire excursions -->
-        <?php
+
+
+      //  Formulaire excursions
+
           if(isset($_POST['formSubmit']))
           {
           $pays = $_POST['Excursions'];
@@ -60,22 +57,23 @@
           exit();
           }
           }
-        ?>
 
-      <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
-          <label for='Excursions'>Choix d'excursion :</label>
-            <select name="Excursions">
-              <option value="">Selectionnez un pays...</option>
-              <option value="Angleterre">Angleterre</option>
-              <option value="Allemagne">Allemagne</option>
-              <option value="Espagne">Espagne</option>
-              <option value="Italie">Italie</option>
-              <option value="Portugal">Portugal</option>
-            </select>
-          <br>
-            Nombre de personnes : <input type="text" name="nombre">
-          <br>
-        <input type="submit" name="name" value="Estimation">
-      </form>
+
+      echo '<form action="<?php echo htmlentities($_SERVER[\'PHP_SELF\']); ?>" method="post">';
+          echo '<label for="Excursions">Choix d\'excursion :</label>';
+          echo '<select name="Excursions" id="Excursions">';
+            echo '<option value="">Selectionnez un pays...</option>';
+            echo '<option value="Angleterre">Angleterre</option>';
+            echo '<option value="Allemagne">Allemagne</option>';
+            echo '<option value="Espagne">Espagne</option>';
+            echo '<option value="Italie">Italie</option>';
+            echo '<option value="Portugal">Portugal</option>';
+          echo '</select>';
+        echo '<br>';
+          echo 'Nombre de personnes : <input type="text" name="nombre">';
+        echo '<br>';
+        echo '<input type="submit" name="name" value="Estimation">';
+      echo '</form>';
+       ?>
     </body>
 </html>
