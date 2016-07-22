11b7d01d17be4d329bc6b338e1005be625d3e755
damien10 <damien.batisse01@gmail.com>
2016-07-22 11:57:34 +0200

    recapitulatif.php final

--- recapitulatif.php
+++ recapitulatif.php
@@ -12,20 +12,25 @@
     <!-- The above 3 meta tags ​must​ come first in the head; any other head content must come ​after​ these tags -->
     <!-- <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico"> -->    <title>Formulaire</title>    <!-- Bootstrap core CSS -->
+    <link rel="icon" href="../../favicon.ico"> -->
+    <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <!-- Modif CSS  -->
-    <link href="css/styleperso.css" rel="stylesheet">    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <link href="css/styleperso.css" rel="stylesheet">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="js/ie-emulation-modes-warning.js"></script>    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->    <!-- Custom styles for this template -->
-    <!-- <link href="" rel="stylesheet"> -->    <!-- jquery libs -->
+    <!-- <link href="" rel="stylesheet"> -->
+    <!-- jquery libs -->
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <title>Formulaire</title>
+    <title>Recapitulatif</title>
   </head>
   <body>
     <header class="well bg-opacity col-md-10 col-md-offset-1">
@@ -94,8 +99,7 @@
             echo '<br><a class="rouge" href="formulaire.php">Retour au formulaire</p>';
             echo '</div>';
             }
-          ?>
-        </div>
+        ?>
       </div>
     </div>
     <!-- Bootstrap core JavaScript ================================================== -->
